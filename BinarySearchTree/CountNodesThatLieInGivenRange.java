da3e1b5749bc92d504f908ca4a6968bb08c0fb92
CodeDev <153368092+SdeCoder72@users.noreply.github.com>
2026-01-24 15:02:03 +0530

    Implement method to count nodes in given range

---
+++ BinarySearchTree/CountNodesThatLieInGivenRange.java
@@ -0,0 +1,10 @@
+
+class Solution {
+    int getCount(Node root, int l, int h) {
+        if(root==null) return 0;
+        if(root.data < l) return getCount(root.right,l,h); 
+        else if(root.data > h) return getCount(root.left,l,h);
+        else return 1+getCount(root.right,l,h)+getCount(root.left,l,h);
+        
+    }
+}
