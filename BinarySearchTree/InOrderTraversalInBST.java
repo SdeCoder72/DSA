6151eccf1558a6dbb098713acef1384f80aa0fe7
CodeDev <153368092+SdeCoder72@users.noreply.github.com>
2026-01-25 07:25:24 +0530

    Implement in-order traversal for BST

---
+++ BinarySearchTree/InOrderTraversalInBST.java
@@ -0,0 +1,15 @@
+class Tree {
+    // Function to return a list containing the inorder traversal of the BST
+    void inorder(Node root, ArrayList<Integer> ans){
+        if(root == null) return;
+        inorder(root.left,ans);
+        ans.add(root.data);
+        inorder(root.right,ans);
+    }
+    ArrayList<Integer> inOrder(Node root) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        inorder(root, ans);
+        return ans;
+        
+    }
+}
