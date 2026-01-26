bed1d08f6a2dc83e76c63f8a5677cb3e60d529b6
CodeDev <153368092+SdeCoder72@users.noreply.github.com>
2026-01-26 07:38:37 +0530

    Implement Maximum Population Year using DAT

---
+++ Line Sweep Technique/MaximumPopulationYear.java
@@ -0,0 +1,25 @@
+
+// 1. Difference Array Technique(DAT)
+class Solution {
+    public int maximumPopulation(int[][] logs) {
+        int[] diffArr = new int[2051];
+        for(int[] log : logs){
+            int start = log[0];
+            int end = log[1];
+
+            diffArr[start] += 1;
+            diffArr[end] -= 1;
+        }
+        int maxPop = 0;
+        int currPop = 0;
+        int minYear = 2050;
+        for(int i=1950; i<2051; i++){
+            currPop += diffArr[i];
+            if(currPop > maxPop){
+                maxPop = currPop;
+                minYear = i;
+            }
+        }
+        return minYear;
+    }
+}
