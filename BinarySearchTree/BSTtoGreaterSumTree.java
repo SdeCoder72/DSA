39caef5075cdca8fc4a53cdfc2bfabeee5881d80
CodeDev <153368092+SdeCoder72@users.noreply.github.com>
2026-01-26 18:55:39 +0530

    Implement BST to Greater Sum Tree transformation
    Added implementation for transforming a BST to a greater sum tree in a new class.

--- BinarySearchTree/BSTtoGreaterSumTree.java
+++ BinarySearchTree/BSTtoGreaterSumTree.java
@@ -1,4 +1,6 @@
-
+//T.C - O(n)
+// A.S - O(h)
+//leetCode 
 class Solution {
     int sum = 0;
     public void solve(TreeNode root){
@@ -13,3 +15,24 @@ class Solution {
         return root;
     }
 }
+
+// gfg
+class Solution {
+    public static int sum;
+    public static void solve(Node root){
+        if(root == null) return;
+        solve(root.right);
+        int val = root.data;
+        root.data = sum;
+        sum += val;
+        solve(root.left);
+    }
+
+    public static void transformTree(Node root) {
+        // code here
+        sum = 0;
+        solve(root);
+        
+    }
+}
+    
