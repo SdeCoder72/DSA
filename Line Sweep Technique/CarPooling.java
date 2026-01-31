d18897719ad8d9512f78848d17c401fef61d0b97
CodeDev <153368092+SdeCoder72@users.noreply.github.com>
2026-01-31 06:34:08 +0530

    Add carPooling method with Difference Array technique
    Implemented car pooling using Difference Array technique for improved space efficiency.

--- Line Sweep Technique/CarPooling.java
+++ Line Sweep Technique/CarPooling.java
@@ -1,3 +1,7 @@
+
+// Line Sweep Technique 
+// Time Complexity - O(n)
+// Space Complexity - O(n)
 class Solution {
     public Map<Integer, Integer> events = new TreeMap<>();
     public boolean carPooling(int[][] trips, int capacity) {
@@ -8,6 +12,27 @@ class Solution {
         }
         int currNum = 0;
         for(int delta : events.values()){
+            currNum += delta;
+            if(currNum > capacity) return false;
+        }
+        return true;
+    }
+}
+
+// Difference Array technique 
+// Time Complexity - O(n)
+// Space Complexity - O(1)
+class Solution {
+
+    public boolean carPooling(int[][] trips, int capacity) {
+        int[] events = new int[1001];
+        for(int[] trip : trips){
+            int delta = trip[0];
+            events[trip[1]] += delta;
+            events[trip[2]] += -delta;
+        }
+        int currNum = 0;
+        for(int delta : events){
             currNum += delta;
             if(currNum > capacity) return false;
         }
