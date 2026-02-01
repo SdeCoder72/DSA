67b2eada9baa3910e79e15ff9e8097562b523adf
CodeDev <153368092+SdeCoder72@users.noreply.github.com>
2026-02-01 16:42:02 +0530

    Implement Morris Inorder Traversal for binary tree

---
+++ TreeDS/Morris_InorderTraversal.java
@@ -0,0 +1,30 @@
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        TreeNode curr = root;
+        while(curr != null){
+            if(curr.left != null){
+                // find pred
+                TreeNode pred = curr.left;
+                while(pred.right != null && pred.right != curr){
+                    pred = pred.right;
+                }
+                if(pred.right == null){ // link
+                 pred.right = curr;
+                 curr = curr.left;
+                }
+                else{ // unlink
+                    pred.right = null;
+                    ans.add(curr.val);
+                    curr = curr.right;
+                }
+            }
+            else{
+                ans.add(curr.val);
+                curr = curr.right;
+            }
+        }
+        return ans;
+    }
+}
