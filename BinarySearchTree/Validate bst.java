6e88dbe8ebefc1d03c5adcc8bac89141cbc81c35
CodeDev <153368092+SdeCoder72@users.noreply.github.com>
2026-02-01 17:19:57 +0530

    Implement Morris traversal for BST validation

--- BinarySearchTree/Validate bst.java
+++ BinarySearchTree/Validate bst.java
@@ -9,3 +9,39 @@ class Solution {
         return l && r;
     }
 }
+
+// Morris traversal
+
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        
+        long prev = Long.MIN_VALUE;
+        TreeNode curr = root;
+        while(curr != null){
+            if(curr.left != null){
+                // find pred
+                TreeNode pred = curr.left;
+                while(pred.right != null && pred.right != curr){
+                    pred = pred.right;
+                }
+
+                if(pred.right == null){ // link
+                 pred.right = curr;
+                 curr = curr.left;
+                }
+                else{ // unlink
+                    pred.right = null;
+                    if(curr.val <= prev) return false;
+                    prev = curr.val;
+                    curr = curr.right;
+                }
+            }
+            else{
+                if(curr.val <= prev) return false;
+                prev = curr.val;
+                curr = curr.right;
+            }
+        }
+        return true;
+    }
+}
