2cce8cc84000bb653260aab91f52387a2a5f74fd
CodeDev <153368092+SdeCoder72@users.noreply.github.com>
2026-02-03 21:50:55 +0530

    Update TwoSum.java

--- 07_Arrays_Java/TwoSum.java
+++ 07_Arrays_Java/TwoSum.java
@@ -27,3 +27,20 @@ public class TwoSum {
     }
 
 }
+
+// Approach - 2
+// Time Complexity - O(n )
+// Space Complexity - O(n)
+class Solution {
+    boolean twoSum(int arr[], int target) {
+        // code here
+        HashSet<Integer> set = new HashSet<>();
+        for(int e : arr){
+            int rem = target - e;
+            if(set.contains(rem)) return true;
+            set.add(e);
+        }
+        return false;
+    }
+}
+
