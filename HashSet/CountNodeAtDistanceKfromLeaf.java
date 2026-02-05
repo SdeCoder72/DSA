128f482921aff3b88bc24916e8031d4706993a22
CodeDev <153368092+SdeCoder72@users.noreply.github.com>
2026-02-05 23:49:43 +0530

    Update CountNodeAtDistanceKfromLeaf.java

--- HashSet/CountNodeAtDistanceKfromLeaf.java
+++ HashSet/CountNodeAtDistanceKfromLeaf.java
@@ -1,6 +1,6 @@
 // Approach - 1 Brute force 
-// Time Complexity - O(n * h)  n = traversal, h = depth of the leaf
-// Space Complexity - O(n * h)
+// Time Complexity - O(n)  n = traversal
+// Space Complexity - O(n * h)  h = depth of the leaf
 
 class Solution {
     List<List<Node>> paths = new ArrayList<>();
@@ -26,6 +26,35 @@ class Solution {
                 result.add(path.get(n-k-1));
             }
         }
+        return result.size();
+    }
+}
+
+
+// Approach - 2 optimal 
+// Time Complexity - O(n)  n = traversal 
+// Space Complexity - O(h). h = depth of the leaf
+
+class Solution {
+    
+    void solve(Node root, List<Node> temp, int idx, int k, HashSet<Node> result){
+        if(root == null) return;
+        temp.add(root);
+        if(root.left == null && root.right == null){
+            if(idx - k >= 0){
+                result.add(temp.get(idx-k));
+            }
+        }
+        solve(root.left, temp, idx+1, k, result);
+        solve(root.right, temp, idx+1, k, result);
+        temp.remove(temp.size()-1);
+    }
+    int kthFromLeaf(Node root, int k) {
+        // code here
+        List<Node> temp = new ArrayList<>();
+        HashSet<Node> result = new HashSet<>();
+        solve(root, temp, 0, k, result);
+        
         return result.size();
     }
 }
