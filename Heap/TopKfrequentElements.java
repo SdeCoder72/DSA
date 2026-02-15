1b749f98159f7e5fee6c8f6f60cccbfd08194595
CodeDev <153368092+SdeCoder72@users.noreply.github.com>
2026-02-15 18:14:05 +0530

    Create TopKfrequentElements.java

---
+++ Heap/TopKfrequentElements.java
@@ -0,0 +1,34 @@
+class Pair implements Comparable<Pair>{
+    int ele;
+    int freq;
+    Pair(int ele, int freq) {
+        this.ele = ele;
+        this.freq = freq;
+    }
+    
+    public int compareTo(Pair p) {
+            if(this.freq == p.freq) return Integer.compare(this.ele, p.ele);
+          return Integer.compare(this.freq, p.freq);  
+    }
+}
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int ele : nums) {
+            map.put(ele, map.getOrDefault(ele, 0) + 1);
+        }
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        for(int val : map.keySet()){
+            int freq = map.get(val);
+            pq.add(new Pair(val, freq));
+            if(pq.size() > k) pq.remove();
+        }
+        int[] ans = new int[k];
+        int i = 0;
+        while(!pq.isEmpty()) {
+            Pair top = pq.remove();
+            ans[i++] = top.ele;
+        }
+        return ans;
+    }
+              }
