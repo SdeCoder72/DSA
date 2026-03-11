b9b769ec08023f5fa83e8b7b7c4e0b3d21ddb04a
CodeDev <153368092+SdeCoder72@users.noreply.github.com>
2026-03-11 20:54:12 +0530

    Add method to swap numbers using XOR
    Implement a method to swap two numbers using bit manipulation.

---
+++ Bit Manipulation/Swap Numbers.java
@@ -0,0 +1,15 @@
+// Time Complexity - O(1)
+// Space Complexity - O(1)
+
+class Solution {
+    static List<Integer> get(int a, int b) {
+        List<Integer> l = new ArrayList<>();
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        
+        l.add(a);
+        l.add(b);
+        return l;
+    }
+}
