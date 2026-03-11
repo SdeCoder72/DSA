02eba1759d05706066dad1426a3bfa1eb9211e77
CodeDev <153368092+SdeCoder72@users.noreply.github.com>
2026-03-11 20:33:02 +0530

    Add Single Number solution using bit manipulation
    Implement a solution to find the single number in an array using bit manipulation.

---
+++ Bit Manipulation/Single Number.java
@@ -0,0 +1,10 @@
+// Time Complexity - O(n)
+// Space Complexity - O(1)
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result= 0;
+        for(int ele : nums) result ^= ele;
+        return result;
+    }
+}
