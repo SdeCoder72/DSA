b52cf70675f84c639896e9f3a94e29f86eecde3e
CodeDev <153368092+SdeCoder72@users.noreply.github.com>
2026-03-22 09:47:40 +0530

    Implement tabulation for min cost climbing stairs
    Added tabulation method for minimum cost climbing stairs.

--- Dynamic Programming/Min Cost Climbing Stairs.java
+++ Dynamic Programming/Min Cost Climbing Stairs.java
@@ -1,3 +1,4 @@
+// Recursion + memoization
 // Time Complexity - O(n)
 // Space Complexity - O(n)
 
@@ -13,5 +14,21 @@ class Solution {
         t = new int[n];
         Arrays.fill(t, -1);
         return Math.min(minCost(0, cost), minCost(1, cost));
+    }
+}
+
+// Tabulation (Iterative)
+// Time Complexity - O(n)
+// Space Complexity - O(n)
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] t = new int[n];
+        t[0] = cost[0];
+        t[1] = cost[1];
+        for(int i = 2; i<n; i++) {
+            t[i] = cost[i] + Math.min(t[i-1], t[i-2]);
+        }
+        return Math.min(t[n-1], t[n-2]);
     }
 }
