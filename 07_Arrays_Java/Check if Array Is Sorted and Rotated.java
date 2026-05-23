f78b5ee2ac730fdddb26b39a2e16779dcc4df035
CodeDev <153368092+SdeCoder72@users.noreply.github.com>
2026-05-23 23:46:57 +0530

    Implement minMaxPairSum to minimize max pair sum
    Implement a method to check if an array is sorted and rotated.

---
+++ 07_Arrays_Java/Check if Array Is Sorted and Rotated.java
@@ -0,0 +1,13 @@
+// Time complexity - O(n)
+// Space complexity - O(1)
+
+class Solution {
+    public boolean check(int[] nums) {
+        int n= nums.length;
+        int peak = 0;
+        for(int i = 0; i<n; i++){
+            if(nums[i] > nums[(i+1)%n]) peak++;
+        }
+        return peak <= 1;
+    }
+}
